86196440cfa53b5e69c5085b92f2906fbb805297
sirlocust <jua56yu@hotmail.com>
2020-08-05 19:52:50 -0500

    encasulapcion tema

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,14 +1,17 @@
 public class Doctor {
     static int id = 0;
+    String email;
     String name;
+
     String speciality;
 
     Doctor(){
         System.out.println("construyendo el objeto doctor");
-        id++;
     }
-    Doctor(String name){
+    Doctor(String name, String speciality){
         this.name = name;
+        this.speciality = speciality;
+        id++;
     }
     public void showName(){
         System.out.println(this.name);
